9cea8839f88e2059ab02838b4b41acacf6a75e06
Peter Stachowski <peterstac@gmail.com>
2014-08-18 13:57:18 -0400

    Initial cut of swift

--- install.sh
+++ install.sh
@@ -2,18 +2,30 @@
 
 #set -vx
 
-if [ $# != 1 ] ; then
-  echo "Usage: $0 <all|tag>"
+if [ $# -lt 1 ] ; then
+  echo "Usage: $0 <all|tag> [keyfile]"
   exit 1;
 fi
 
 TAGS=$1
-TAGS_ARG="--tags=$1"
+TAGS_ARG="--tags=$TAGS"
 if [ x"$TAGS" = x"all" ] || [ x"$TAGS" = x"All" ] || [ x"$TAGS" = x"ALL" ]
 then
   TAGS_ARG=
 fi
 
+KEYFILE=$2
+KEYFILE_ARG="--private-key=~/.ssh/id_rsa"
+if [ ! x"$KEYFILE" = x"" ]
+then
+  if [ ! -f "$KEYFILE" ]
+  then
+    echo "Key file '$KEYFILE' not found - using default"
+  else
+    KEYFILE_ARG="--private-key=$KEYFILE"
+  fi
+fi
+
 echo Installing $TAGS
-ansible-playbook -i hosts site.yml --private-key=~/.ssh/id_rsa --sudo -K --tags=$TAGS
+ansible-playbook -i hosts site.yml $KEYFILE_ARG --sudo -K $TAGS_ARG
 
